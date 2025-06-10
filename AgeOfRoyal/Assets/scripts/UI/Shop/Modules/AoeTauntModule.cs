a07ed3dd00fc89890fbee0cbfac9e2d085a40f7a
JausGames <71459206+JausGames@users.noreply.github.com>
2025-06-10 13:22:13 +0200

    Update AoeTauntModule.cs

--- AgeOfRoyal/Assets/scripts/UI/Shop/Modules/AoeTauntModule.cs
+++ AgeOfRoyal/Assets/scripts/UI/Shop/Modules/AoeTauntModule.cs
@@ -4,7 +4,7 @@
 [CreateAssetMenu(fileName = "___ - MOD AOE Taunt", menuName = "Unit Modules/AOE Taunt", order = 4)]
 public class AoeTauntModule : AoeUnitModule
 {
-    public override string Description => $"Foes within {radius}m target this unit";
+    public override string Description => $"{(picking.SameTeam == Target.Friends ? "Allies" : picking.SameTeam == Target.Foes ? "Foes" : "Units")} within {radius}m target this unit";
 
     override protected void ApplyEffect(Hitable target, MinionCombat owner)
     {
