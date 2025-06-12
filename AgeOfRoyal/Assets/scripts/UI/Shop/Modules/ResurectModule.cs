d7498bfe3c39f9b83ebfe860b61ad9b40565284c
JausGames <71459206+JausGames@users.noreply.github.com>
2025-06-12 15:51:59 +0200

    Create ResurectModule.cs

---
+++ AgeOfRoyal/Assets/scripts/UI/Shop/Modules/ResurectModule.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Linq;
+using Unity.Netcode;
+using UnityEngine; 
+
+[System.Serializable]
+abstract public class ResurectModule : UnitModule
+{
+    [SerializeField] protected float cooldown = 5f;
+    [SerializeField] protected float maxHealthFlat = 0;
+    [SerializeField] protected float healthPercent = 1f;
+    [SerializeField] protected TargetPicking picking;
+
+    float lastUsed = 0f;
+
+    public float Cooldown => cooldown; 
+    public TargetPicking Picking { get => picking; set => picking = value; }
+    public abstract bool VfxLoop => false;
+    public override int Use(MinionCombat owner, int maxTargetOverride = -1)
+    {
+        if (!owner.IsServer) return 0;
+        var maxTarget = maxTargetOverride == -1 ? picking.MaxTarget : maxTargetOverride;
+        var unitsManager = owner.FindFirstObjectByType<UnitsManager>();
+        List<Minion> minions = picking.PickTargets(unitsManager.Deads).Take(maxTarget).ToList(); 
+
+        owner.Owner.PlayVfx(OnSelfVfx);
+        owner.Owner.PlayModuleOnSelfVfxClientRpc(ID, owner.NetworkObjectId);
+
+        var nbTouched = 0;
+        foreach (var h in minions)
+        {
+            unitsManager.Resurect(h, healthPercent, maxHealthFlat);
+        } 
+        return nbTouched;
+    } 
+    public override UnitModule Clone()
+    {
+        var clone = Instantiate(this);
+        clone.ID = this.ID;
+
+        // Optional deep clone if TargetPicking is mutable
+        clone.Picking = picking != null ? picking.Clone() : null;
+
+        return clone;
+    }
+}
