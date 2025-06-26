c1cb9fc824d86b4429076350f79a69a305dd6b90
JausGames <71459206+JausGames@users.noreply.github.com>
2025-06-26 14:15:21 +0200

    Update UnitUpgrade.cs

--- AgeOfRoyal/Assets/scripts/Player/UnitUpgrade.cs
+++ AgeOfRoyal/Assets/scripts/Player/UnitUpgrade.cs
@@ -14,25 +14,28 @@ public class UpgradeDescription
 [CreateAssetMenu(fileName = "Unit Upgrade", menuName = "Buttons/Unit Upgrade", order = 1)]
 public class UnitUpgrade : ScriptableObject
 {
+    [Header("Global")]
     [SerializeField] public int ID = -1;
     [SerializeField] string perkName; 
-    [SerializeField] UnitBuff buff = new UnitBuff();  
-    [SerializeField] List<UnitModule> modules;
-    [SerializeField] List<UnitAction> actions;
-    [SerializeField] List<BuffApplyTrigger> triggers = null;
     [SerializeField] int cost;
     [SerializeField] List<UpgradeDescription> descriptions;
     [SerializeField] List<UnitWithoutState> target;
     [SerializeField] Sprite image;
+    [Header("Bonuses")]
+    [SerializeField] UnitBuff buff = new UnitBuff();  
+    [SerializeField] List<UnitModule> modules;
+    [SerializeField] List<UnitAction> actions;
+    [SerializeField] List<BuffApplyTrigger> triggers = null;
 
     public string Name => perkName; 
     public UnitBuff Buff => buff;  
-    public List<UnitModule> Modules => modules;
-    public List<UnitAction> Actions => actions;
-    public List<BuffApplyTrigger> Triggers => triggers;
     public int Cost => cost;
     public List<UpgradeDescription> Descriptions => descriptions;
     public List<UnitWithoutState> Target => target; 
     public Sprite Image => image; 
+    
+    public List<UnitModule> Modules => modules;
+    public List<UnitAction> Actions => actions;
+    public List<BuffApplyTrigger> Triggers => triggers;
 }
 
